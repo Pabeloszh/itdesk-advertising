c31b9679cca25ed0d5344be288f56e73f0e027d2
unknown <Pabeloszh@gmail.com>
2021-03-18 12:31:54 +0100

    offers route added (/uslugi)

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,18 @@
 <template>
     <Navbar/>
-    <MainSlider/>
-    <Services/>
-    <ContactUs/>
-    <AboutUs/>
-    <Blog/>
-    <ContactForm/>
+    <router-view></router-view>
     <Footer/>
 </template>
 
 <script>
 // @ is an alias to /src
 import Navbar from "@/components/Navbar/Navbar.vue";
-import MainSlider from "@/components/MainSlider/MainSlider.vue";
-import Services from "@/components/Services/Services.vue";
-import ContactUs from "@/components/ContactUs/ContactUs.vue"
-import AboutUs from "@/components/AboutUs/AboutUs.vue"
-import Blog from "@/components/Blog/Blog.vue"
-import ContactForm from "@/components/ContactForm/ContactForm.vue"
 import Footer from "@/components/Footer/Footer.vue"
 
 export default {
   name: "App",
   components: {
     Navbar,
-    MainSlider,
-    Services,
-    ContactUs,
-    AboutUs,
-    Blog,
-    ContactForm,
     Footer
   }
 };
---
+++ src/components/OffersBox/OffersBox.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="off-box">
+        <div class="box">
+            <img src="https://agencja-interaktywna.opole.pl/wp-content/uploads/2020/10/ikona1-p.svg" alt="icon">
+            <div>
+                <h5>MARKETING INTERNETOWY</h5>
+                <p>Wprowadzimy Twoją Markę na rynek, sprawimy, że usłyszy o niej cały świat. <a href="/opieka-marketingowa/">Opieka marketingowa</a>, <a href="/google-ads/">Google Ads</a>, <a href="/audyt-strony/">Audyt Strony</a>, <a href="/pozycjonowanie/">Pozycjonowanie</a>, <a href="/social-media/">Social Media</a>, <a href="/blog-firmowy/">Blog Firmowy</a>, <a href="/mailing/">Mailing</a>, <a href="/marketing-szeptany/">Marketing szeptany</a></p>
+            </div>
+        </div>
+        <div class="box">
+            <img src="https://agencja-interaktywna.opole.pl/wp-content/uploads/2020/10/ikona2-p-1.svg" alt="icon">
+            <div>
+                <h5>STRONY I SKLEPY INTERNETOWE</h5>
+               <p>Od ponad 10 lat tworzymy strony internetowe, portale i sklepy dla firm i klientów indywidualnych. <a href="/systemy-zarzadzania-trescia-cms/">Systemy zarządzania treścią CMS</a>, <a href="https://agencja-interaktywna.opole.pl/strony-internetowe/">Profesjonalne strony internetowe</a>, <a href="/opieka-nad-strona-www/">Opieka nad stroną www</a>, <a href="/sote-shop/">Sklep Sote Shop</a>, <a href="/presta-shop/">Sklep Presta Shop</a>, <a href="/magento-2/">Sklep Magento</a></p>
+            </div>
+        </div>
+        <div class="box">
+            <img src="https://agencja-interaktywna.opole.pl/wp-content/uploads/2020/10/ikona3-p.svg" alt="icon">
+            <div>
+                <h5>HOSTING I DOMENY</h5>
+                <p><a href="https://agencja-interaktywna.opole.pl/hosting-i-domeny/">Hosting</a>, to dedykowane miejsce na serwerze dla Twojej strony, sklepu internetowego lub aplikacji. Hosting stron i aplikacji, Hosting CMS, Dedykowana administracja, Pakiety Email.</p>
+            </div>
+        </div>
+        <div class="box">
+            <img src="https://agencja-interaktywna.opole.pl/wp-content/uploads/2020/10/ikona4-p.svg" alt="icon">
+            <div>
+                <h5>OPROGRAMOWANIE DEYDYKOWANE</h5>
+                <p>Zajmujemy się pisaniem systemów dedykowanych dla naszych klientów.</p>
+            </div>
+        </div>
+        <div class="box">
+            <img src="https://agencja-interaktywna.opole.pl/wp-content/uploads/2020/10/ikona5-p.svg" alt="icon">
+            <div>
+                <h5>APLIKACJE MOBILNE</h5>
+                <p>Tworzymy skuteczne aplikację na najpopularniejsze obecnie platformy mobilne</p>
+            </div>
+        </div>
+        <div class="box">
+            <img src="https://agencja-interaktywna.opole.pl/wp-content/uploads/2020/10/ikona-media-1.png" alt="icon">
+            <div>
+                <h5>MULTIMEDIA, VIDEO &amp; MOTION</h5>
+                <p>Tworzymy multimedialne prezentacje, animacje oraz filmy promocyjne, aby zwiększyć skuteczność prowadzonych działań marketingowych. <a href="https://www.youtube.com/">YouTube</a>, Vimeo.</p>
+            </div>
+        </div>
+        <div class="box">
+            <img src="https://agencja-interaktywna.opole.pl/wp-content/uploads/2020/10/ikona-chmura.png" alt="icon">
+            <div>
+                <h5>USŁUGI W CHMURZE</h5>
+                <p>Jesteśmy partnerem dostawców usług w chmurze tj. Google Apps oraz Microsoft Office 365 / Exchange OnLine. Zapewniamy niezawodne narzędzia do pracy zespołowej.</p>
+            </div>
+        </div>
+        <div class="box">
+            <img src="https://agencja-interaktywna.opole.pl/wp-content/uploads/2020/10/ikona-szkolenia.png" alt="icon">
+            <div>
+                <h5>SZKOLENIA</h5>
+                <p>Prowadzimy szkolenia dla firm i klientów indywidualnych.</p>
+            </div>
+        </div>
+  </div>
+</template>
+<script>
+export default {
+    name:"OffersBox"
+}
+</script>
+<style scoped lang="scss" src="./OffersBox.style.scss">
+
+</style>
