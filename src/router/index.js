7737622f95132f3a8b46eda0b8389b875bd620a8
unknown <Pabeloszh@gmail.com>
2021-03-22 11:02:59 +0100

    whisper marketing route added

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,8 @@ import SiteAudit from "../views/SiteAudit.vue";
 import CompanyBlogDesc from "../views/CompanyBlog.vue";
 import Mailing from "../views/Mailing.vue";
 import Positioning from "../views/Positioning.vue";
-import SocialMedia from "../views/SocialMedia.vue"
+import SocialMedia from "../views/SocialMedia.vue";
+import WhisperMarketing from "../views/WhisperMarketing.vue"
 
 const routes = [
   {
@@ -60,6 +61,11 @@ const routes = [
     path: "/social-media",
     name: "SocialMedia",
     component: SocialMedia
+  },
+  {
+    path: "/marketing-szeptany",
+    name: "WhisperMarketing",
+    component: WhisperMarketing
   }
 ];
 
