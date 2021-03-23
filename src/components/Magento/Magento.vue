6244135250f6eede4821fa69f83535221e46fe6f
unknown <Pabeloszh@gmail.com>
2021-03-23 14:12:00 +0100

    presta shop route added

---
+++ src/components/Magento/Magento.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="mshop-desc">
+      <p>Magento, jest rozbudowanym skryptem, pozwala tworzyć funkcjonalne, złożone sklepy internetowe w oparciu o sprzedaż polską i zagraniczną. Decydując się na ten sklep, warto podjąć współpracę z ekspertami, którzy mają doświadczenie w budowaniu sklepów opartych na tym silniku.</p>
+      <div class="mshop-box">
+          <div>
+            <h2>Główne cechy Magento</h2>
+            <p>silnik przeznaczony dla właścicieli średnich i dużych sklepów (min. kilkadziesiąt produktów)</p>
+            <p>sprawdzony na rynkach polskich i międzynarodowych</p>
+            <p>wyróżnia się rozbudowaną funkcjonalnością</p>
+            <p>wymaga zaawansowanej wiedzy wdrożeniowca</p>
+            <p>posiada dość złożony panel administracyjnym</p>
+            <p>możliwość stworzenia sklepu internetowego w technologii responsive</p>
+            <p>wiele darmowych dodatków</p>
+            <p>definiowane uprawnienia administratorów</p>
+            <p>możliwość użycia wielu walut</p>
+            <p>możliwość zarządzania wieloma językami</p>
+            <p>funkcja multisklepu – możliwość zarządzania kilkoma sklepami z jednego panelu.</p>
+          </div>
+          <div>
+            <h2>Integracja Magento</h2>
+            <p>Magento, to złożony system pozwalający na integrację z popularnymi programami księgowo – magazynowymi, takimi jak <a href="https://itdesk.eu/usluga/enova365-erp/">Enova</a>, Subiekt, WF-MAG.<br> Profesjonalnie przeprowadzona integracja z subiektem pozwala na zautomatyzowanie pewnych czynności:</p>
+            <p>integracja produktów</p>
+            <p>integracja i możliwość edytowania cech produktu</p>
+            <p>integracja kontrahentów</p>
+            <p>synchronizacja danych</p>
+            <p>czytelny sposób przesyłania informacji o stanach magazynowych</p>
+            <p>Rozwiązanie to dedykujemy właścicielom większych sklepów internetowych, którym zależy na pełnej integracji i szybszej obsłudze sklepu.</p>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
