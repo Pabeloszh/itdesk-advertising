16910fb04bd5f9a6184f9fcc67021d2c4905f904
unknown <Pabeloszh@gmail.com>
2021-04-06 12:44:09 +0200

    map iframe added to contact

--- src/components/Contact/Contact.vue
+++ src/components/Contact/Contact.vue
@@ -17,7 +17,7 @@
         <input type="button" value="WYŚLIJ WIADOMOŚĆ" />
       </form>
     </div>
-    <div></div>
+    <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2529.0840226938662!2d17.891317915739105!3d50.662700479505226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471053a33e4af861%3A0xa2554556906ca6d1!2sitDesk!5e0!3m2!1spl!2spl!4v1617705264730!5m2!1spl!2spl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe></div>
   </div>
 </template>
 
