3072dd20f9177f79559406c8cfcc440905e182fc
unknown <Pabeloszh@gmail.com>
2021-04-07 13:21:16 +0200

    portfolio filtering function added + pagination

--- src/components/Portfolio/Portfolio.vue
+++ src/components/Portfolio/Portfolio.vue
@@ -2,14 +2,38 @@
   <Title :groupedProps="titleData" />
   <div class="port-container" v-if="projectsLoaded">
       <div class="filters">
-          *filters*
+          <div style="background-color:#f9f9f9">
+              <p @click="filter('all', 0)">ALL</p>
+          </div>
+          <div @click="filter('google ads', 1)">
+              <p>GOOGLE ADS</p>
+          </div>
+          <div  @click="filter('oprogramowanie dedykowane', 2)">
+              <p>OPROGRAMOWANIE</p>
+          </div>
+          <div @click="filter('sklep internetowy', 3)">
+              <p>SKLEPY INTERNETOWE</p>
+          </div>
+          <div @click="filter('social media', 4)">
+              <p>SOCIAL MEDIA</p>
+          </div>
+          <div @click="filter('strona internetowa', 5)">
+              <p>STRONY INTERNETOWE</p>
+          </div>
       </div>
       <div class="projects">
-        <div  v-for="(item, index) in ourProjects.data.items"
+        <div  v-for="(item, index) in currItems"
         :key="index" @click="$router.push({ name: 'Project', params: { name: item.fields.title.replace(/\s+/g, '-').toLowerCase() } })">
             <img :src="item.fields.mainPhoto" alt="">
             <h3>{{item.fields.title}}</h3>
         </div>
+      </div>
+      <div class="pages" v-if="projectsLoaded">
+          <p v-if="currPage !== 1" @click="currPage--">Prev</p>
+          <p v-for="(n,index) in pages" :key="index" @click="currPage = n" :style="currPage === n ? {color: '#ef7a07'} : {color: '#999'}">
+              {{n}}
+          </p>
+          <p v-if="currPage !== pages" @click="currPage++">Next</p>
       </div>
   </div>
   <ContactForm />
@@ -28,13 +52,18 @@ export default {
     data() {
         return {
             titleData: {
-            backgroundPhoto: {
-                backgroundImage:
-                "url(https://agencja-interaktywna.opole.pl/wp-content/uploads/2020/09/back11.jpg)"
+                backgroundPhoto: {
+                    backgroundImage:
+                    "url(https://agencja-interaktywna.opole.pl/wp-content/uploads/2020/09/back11.jpg)"
+                },
+                desc: "Wybrane",
+                title: "REALIZACJE"
             },
-            desc: "Wybrane",
-            title: "REALIZACJE"
-            }
+            pages: 0,
+            itemsOnPage: 20,
+            currPage: 1,
+            currItems: [],
+            filteredItems:[]
         }
     },
     computed: {
@@ -45,6 +74,39 @@ export default {
     },
     methods: {
          ...mapMutations(["ourProjects", "projectsLoaded"]),
+         filter(params, i){
+            this.currPage = 1;
+            let divs = document.querySelectorAll('.filters div');
+            divs.forEach(div => div.style.backgroundColor = "#fff")
+            divs[i].style.backgroundColor = "#f9f9f9"
+
+             if(params === 'all') {
+                this.filteredItems = [];
+                this.pages = Math.ceil(this.ourProjects.data.items.length / this.itemsOnPage);       
+                this.currItems = this.ourProjects.data.items.slice((this.currPage - 1) * 20, this.itemsOnPage);
+             } else {
+                this.filteredItems = this.ourProjects.data.items.filter((item)=>
+                    item.fields.type.toLowerCase().includes(params)
+                )
+                this.pages = Math.ceil(this.filteredItems.length / this.itemsOnPage);
+                this.currItems = this.filteredItems.slice((this.currPage - 1) * 20, this.itemsOnPage);
+            }
+        }
+    },
+    watch:{
+        projectsLoaded: {
+            handler: function(){
+                if(this.projectsLoaded) {
+                    this.pages = Math.ceil(this.ourProjects.data.items.length / this.itemsOnPage);       
+                    this.currItems = this.ourProjects.data.items.slice((this.currPage - 1) * 20, this.itemsOnPage);
+                }
+            },
+            immediate: true
+        },
+        currPage: function(val){
+            if(this.filteredItems.length === 0) this.currItems = this.ourProjects.data.items.slice((val - 1) * 20, this.currPage < (this.ourProjects.data.items.length / this.itemsOnPage) ? this.currPage * this.itemsOnPage : this.itemsOnPage * (this.ourProjects.data.items.length / this.itemsOnPage));
+            if(this.filteredItems.length !== 0) this.currItems = this.filteredItems.slice((val - 1) * 20, this.currPage < (this.ourProjects.data.items.length / this.itemsOnPage) ? this.currPage * this.itemsOnPage : this.itemsOnPage * (this.ourProjects.data.items.length / this.itemsOnPage));
+        }
     }
 }
 </script>
